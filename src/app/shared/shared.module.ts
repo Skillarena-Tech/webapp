aa914a50e50df3ea282a5d38003687cb94ce5310
Ashutosha Sahu <ashutoshsahu2000@gmail.com>
2023-12-06 00:14:09 +0530

    styles and components added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { PostCardComponent } from './post-card/post-card.component';
+import { SideCardComponent } from './side-card/side-card.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    PostCardComponent,
+    SideCardComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    PostCardComponent,
+    SideCardComponent
   ],
 })
 export class SharedModule { }
